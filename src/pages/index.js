9d662ce2ceb6f1233a986078691cd4a6cb1af5de
Joseph M <jmedina1985@hotmail.com>
2021-11-12 13:00:27 -0800

    getting started

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Layout from '../components/Layout'
+import Posts from '../components/Posts'
+import { graphql } from 'gatsby'
+const IndexPage = () => {
+  return <h2>gatsby mdx blog starter</h2>
+}
+
+export default IndexPage
