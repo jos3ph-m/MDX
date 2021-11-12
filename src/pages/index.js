baad8affba644a45ae2412c3eccba63486a64783
Joseph M <jmedina1985@hotmail.com>
2021-11-12 13:14:42 -0800

    getting started

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 const IndexPage = () => {
-  return <h2>gatsby mdx blog starter</h2>
+  return <Layout></Layout>
 }
 
 export default IndexPage
