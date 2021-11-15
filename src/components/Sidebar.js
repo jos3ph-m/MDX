6c76634333e9f1742aee5ddfb5c47b3feb719472
Joseph M <jmedina1985@hotmail.com>
2021-11-14 16:41:30 -0800

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import Links from '../constants/links'
 import Categories from '../components/Categories'
 import { IoMdClose } from 'react-icons/io'
 
-const Sidebar = () => {
+const Sidebar = ({ isOpus }) => {
   return (
     <aside className={`sidebar showSidebar`}>
       <button className="close-btn">
