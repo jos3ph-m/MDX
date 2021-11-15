9b237d266ef2bd7b785bb4fd7816447d16864eb1
Joseph M <jmedina1985@hotmail.com>
2021-11-14 16:42:57 -0800

    sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,9 @@ import Sidebar from './Sidebar'
 import Footer from './Footer'
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = React.useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
