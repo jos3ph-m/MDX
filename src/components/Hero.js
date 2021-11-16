ffca3adb3c74f15b83b5891f5c5e491c9d0890fd
Joseph M <jmedina1985@hotmail.com>
2021-11-15 16:14:05 -0800

    sidebar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const Hero = () => {
-  return <header>hero component</header>
+  return <header className="hero">hero component</header>
 }
 
 export default Hero
