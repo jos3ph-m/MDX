179200a10021ee4d476da870ca8fdd0c76dbb67e
Joseph M <jmedina1985@hotmail.com>
2021-11-16 15:07:28 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div>
-        <SocialLinks styleClass="" />
+        <SocialLinks styleClass="footer-icons" />
       </div>
     </footer>
   )
