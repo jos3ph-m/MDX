ac67b25ac7ce0fe4f6e65fd2408a596629ca41d8
Joseph M <jmedina1985@hotmail.com>
2021-11-16 15:06:00 -0800

    footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,7 +20,7 @@ const Layout = ({ children }) => {
       <Navbar toggle={toggle} />
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <main>{children}</main>
-      <Footer></Footer>
+      <Footer />
     </>
   )
 }
