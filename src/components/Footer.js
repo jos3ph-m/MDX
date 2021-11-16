0d01f7a36365af65dc7515409a1657e3fdd3c7a5
Joseph M <jmedina1985@hotmail.com>
2021-11-16 15:06:41 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SocialLinks from '../constants/socialLinks'
 const Footer = () => {
-  return <footer></footer>
+  return <footer className="footer"></footer>
 }
 
 export default Footer
