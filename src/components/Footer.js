e8adbc55c2fec108a0af902db99e950fa82f8713
Joseph M <jmedina1985@hotmail.com>
2021-11-16 15:08:55 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer">
       <div>
         <SocialLinks styleClass="footer-icons" />
-        <p>&copy;{new Date().getFullYear()} MDXBlog. All rights reserved</p>
+        <p>&copy;{new Date().getFullYear()} MDXBlog - All rights reserved</p>
       </div>
     </footer>
   )
