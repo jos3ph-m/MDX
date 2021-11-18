1e604e4ec36ecdc2939bbac88af8b5465eab254d
Joseph M <jmedina1985@hotmail.com>
2021-11-17 16:26:03 -0800

    Error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
-const NotFoundPage = () => <Layout></Layout>
+const NotFoundPage = () => (
+  <Layout>
+    <section></section>
+  </Layout>
+)
 
 export default NotFoundPage
