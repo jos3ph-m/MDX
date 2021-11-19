f89a92343c8f13ddf9cbbaf92bdb1776f2d407fe
Joseph M <jmedina1985@hotmail.com>
2021-11-18 19:34:22 -0800

    Newsletter info

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,6 +7,9 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all the latest stories to your inbox</h2>
           <h4>I wrote to my friends every few weeks</h4>
+          <form className="contact-form">
+            <input type="text" />
+          </form>
         </div>
       </section>
     </Layout>
