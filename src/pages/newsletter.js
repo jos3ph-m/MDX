ace0fbec0ad7ac7f2a67b2fba7542a645461a661
Joseph M <jmedina1985@hotmail.com>
2021-11-18 19:31:48 -0800

    Newsletter info

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section></section>
+    </Layout>
+  )
 }
 
 export default NewsLetter
