a0c08a60c3e2d7a7705327d1035af4113e42f3e4
Joseph M <jmedina1985@hotmail.com>
2021-11-19 16:52:58 -0800

    Newsletter info

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Link } from 'gatsby'
 const Success = () => {
-  return (
-    <h4>success page</h4>
-  )
+  return <Layout>success page</Layout>
 }
 
 export default Success
