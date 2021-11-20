708a6e37d73d6b5533bfff76a6120a851be68b4b
Joseph M <jmedina1985@hotmail.com>
2021-11-19 16:54:27 -0800

    Newsletter info

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,7 +5,12 @@ const Success = () => {
   return (
     <Layout>
       <section className="success-page">
-        <div className="page-center"></div>
+        <div className="page-center">
+          <h2>your submission was recieved!</h2>
+          <Link to="/" className="btn">
+            back home
+          </Link>
+        </div>
       </section>
     </Layout>
   )
