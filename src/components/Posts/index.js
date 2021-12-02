8a7de0deb430f1d9a6e63759cd74df5f9acf3c04
Joseph M <jmedina1985@hotmail.com>
2021-12-01 18:23:33 -0800

    Posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Post from './Post'
 import Banner from '../Banner'
 const Posts = ({ posts, title }) => {
-  return <section className="posts">posts component</section>
+  return (
+    <section className="posts">
+      <h3></h3>
+    </section>
+  )
 }
 
 export default Posts
