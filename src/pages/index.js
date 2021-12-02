241566b4c31b3d1e060d52da22ed79b5f531114c
Joseph M <jmedina1985@hotmail.com>
2021-12-01 18:11:01 -0800

    Frontmatter

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,28 +11,4 @@ const IndexPage = ({}) => {
   )
 }
 
-export const query = graphql`
-  {
-    allMdx(limit: 3, sort: { fields: frontmatter___title, order: DESC }) {
-      nodes {
-        id
-        frontmatter {
-          title
-          author
-          category
-          readTime
-          slug
-          date(formatString: "MMMM, Do YYYY")
-          image {
-            childImageSharp {
-              gatsbyImageData
-            }
-          }
-        }
-        excerpt
-      }
-    }
-  }
-`
-
 export default IndexPage
