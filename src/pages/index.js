13d20ab17f1bbbd01bda22cb656362ff8c0bc5f8
Joseph M <jmedina1985@hotmail.com>
2021-12-01 18:16:56 -0800

    Frontmatter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,37 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = ({}) => {
+
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <Hero showPerson />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childrenImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
