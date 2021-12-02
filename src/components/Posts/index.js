0a358e7fe27f9095368987c2451515cc28f79b86
Joseph M <jmedina1985@hotmail.com>
2021-12-01 18:24:55 -0800

    Posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -5,7 +5,14 @@ const Posts = ({ posts, title }) => {
   return (
     <section className="posts">
       <h3 className="posts-title">{title}</h3>
-      <div className="posts-center"></div>
+      <div className="posts-center">
+        {/* posts column */}
+        <article>
+          {posts.map(post => {
+            return <Post key={post.id} />
+          })}
+        </article>
+      </div>
     </section>
   )
 }
