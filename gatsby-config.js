cd58317504f3bfa9f437ac4d9fd8cf3cd1dfcc76
Joseph M <jmedina1985@hotmail.com>
2021-12-01 18:09:52 -0800

    Frontmatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
         path: `${__dirname}/src/assets`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `posts`,
-    //     path: `${__dirname}/src/posts`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-mdx`,
     //   options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = ({ data }) => {
+const IndexPage = ({}) => {
   return (
     <Layout>
       <Hero showPerson />
