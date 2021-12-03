21238d3b3dd5b037634ed56e1c7f31db7848a6c3
Joseph M <jmedina1985@hotmail.com>
2021-12-03 15:09:42 -0800

    Posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,7 +3,8 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
+const PostsPage = ({ data }) => {
+  connst {allMdx}
   return <h4>posts page</h4>
 }
 
